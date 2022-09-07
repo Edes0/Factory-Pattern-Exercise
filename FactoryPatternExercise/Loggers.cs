9c4567405f36b6c3c5a54ab56204a3bab5f9ae74
Edes0 <90324120+Edes0@users.noreply.github.com>
2022-09-07 10:30:17 +0200

    Add project files.

---
+++ FactoryPatternExercise/Loggers.cs
@@ -0,0 +1,24 @@
+﻿namespace FactoryPatterExercise
+{
+    public class ConsoleLogger
+    {
+        public void PrintToConsole(string logData)
+        {
+            Console.WriteLine(logData);
+        }
+    }
+    public class FileLogger
+    {
+        public void PrintToFile(string logData)
+        {
+            Console.WriteLine(logData);
+        }
+    }
+    public class DataBaseLogger
+    {
+        public void PrintToDataBase(string logData)
+        {
+            Console.WriteLine(logData);
+        }
+    }
+}
---
+++ FactoryPatternExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using FactoryPatterExercise;
+
+DateTime lastLog = DateTime.Now;
+var logType = "Console";
+
+while (true)
+{
+    if (lastLog <= DateTime.Now)
+    {
+        var result = $"Important work was done at {DateTime.Now}";
+        Log(result);
+
+        lastLog = DateTime.Now.AddSeconds(10);
+    }
+}
+
+void Log(string result)
+{
+    if (logType.ToLower() == "console")
+    {
+        var logger = new ConsoleLogger();
+        logger.PrintToConsole(result);
+    }
+
+    if (logType.ToLower() == "file")
+    {
+        var logger = new FileLogger();
+        logger.PrintToFile(result);
+    }
+
+    if (logType.ToLower() == "database")
+    {
+        var logger = new DataBaseLogger();
+        logger.PrintToDataBase(result);
+    }
+}
